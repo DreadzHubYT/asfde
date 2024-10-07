2939ddbc6cda1e1131d7ed4cb28933b70faf2a1c
DreadzHubYT <114454226+DreadzHubYT@users.noreply.github.com>
2024-10-06 17:00:41 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,24 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: '/bare/',
+    bare: {
+        "versions": [
+            "v1",
+            "v2",
+            "v3"
+        ],
+        "language": "NodeJS",
+        "memoryUsage": 10.29,
+        "maintainer": {
+            "email": "ben@benrogo.net",
+            "backend_id": "bare-02-201-8082"
+        },
+        "project": {
+            "name": "bare-server-node",
+            "description": "TOMPHTTP NodeJS Bare Server",
+            "repository": "https://github.com/tomphttp/bare-server-node",
+            "version": "2.0.4"
+        }
+    },
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
